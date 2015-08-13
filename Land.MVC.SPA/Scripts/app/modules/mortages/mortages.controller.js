9b4ed028a5f08ea7e31f058e475a36d96882ab62
Krasimir Nikolov <krasi.nikolov@gmail.com>
2015-08-13 15:13:15 +0300

    Create mortages.controller.js

---
+++ Land.MVC.SPA/Scripts/app/modules/mortages/mortages.controller.js
@@ -0,0 +1,44 @@
+(function () {
+    'use strict';
+
+    angular.module('app.modules.mortages')
+    .controller('MortagesController', MortagesController);
+
+    MortagesController.$inject = ['$modal', 'MortagesService', 'LandPropertiesService'];
+    function MortagesController($modal, mortagesService,landPropertiesService) {
+        //TODO load data on route resolve
+        var vm = this;
+        mortagesService.getMortages()
+        .then(function (mortages) {
+            vm.mortages = mortages;
+        });
+        
+        landPropertiesService.getLandProperties()
+        .then(function (landProperties) {
+            vm.landProperties = landProperties;
+        });
+        
+        //////////////////
+        
+        modalOpen(mortage,isNew){
+            mortage = mortage||{};
+            mortage.isNew = isNew;
+            $modal.open({
+                templateUrl:'/templates/MortageModal',
+                controller: 'MortageModalController',
+                controllerAs: 'vm',
+                backdropClass: 'backdrop-fixed',
+                resolve: {
+                        mortage: function () {
+                            return mortage;
+                        }
+                    }
+            }).result.then(function (){
+                        mortagesService.getMortages()
+                      .then(function (mortages) {
+                         vm.mortages = mortages;
+                    });
+                });
+        }
+    }
+})();
